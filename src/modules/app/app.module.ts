175654d1e696d263469455adc2ec46468ecf88d6
Jeon-man <jhmjhm0106@naver.com>
2023-12-13 09:45:50 +0900

    FEAT: Import modules on app.module

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Modules } from '..';
 
 @Module({
-  imports: [],
+  imports: [...Object.values(Modules).filter(m => m && m !== AppModule)],
   controllers: [AppController],
   providers: [AppService],
 })
