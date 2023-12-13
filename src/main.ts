6255f61688610007e2596b14d4879875998af90d
Jeon-man <jhmjhm0106@naver.com>
2023-12-13 11:33:23 +0900

    FEAT: Add `ValidationPipe`  to global

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
 import { AppModule } from '@module/app';
+import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  const config: ConfigService = app.get(ConfigService);
+  const NODE_ENV = config.get<string>('NODE_ENV');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      transformOptions: { enableImplicitConversion: true, exposeDefaultValues: true },
+      enableDebugMessages: NODE_ENV !== 'production',
+      whitelist: true,
+    }),
+  );
 }
 bootstrap();
