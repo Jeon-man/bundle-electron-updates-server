645e7b04bbcbc0cc3788848a42c7aec35058bbeb
Jeon-man <jhmjhm0106@naver.com>
2023-12-15 11:48:23 +0900

    FEAT: Define model associations

--- src/modules/bundle/models/bundle.manifest.model.ts
+++ src/modules/bundle/models/bundle.manifest.model.ts
@@ -1,13 +1,19 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { BaseManifestModel } from '@util/sequelize';
 import { JSON_STRING } from '@util/sequelize/types';
-import { Column, DataType, Table } from 'sequelize-typescript';
+import {
+  BelongsToMany as BelongsToManyAssociation,
+  HasMany as HasManyAssociation,
+} from 'sequelize';
+import { BelongsToMany, Column, DataType, HasMany, Table } from 'sequelize-typescript';
 import {
   BundlePlatform,
   BundlePlatformList,
   Bundler,
   ModuleFederationConfig,
 } from '../bundle.types';
+import { BundleAsset } from './bundle.asset.model';
+import { BundleManifest_Asset } from './manifest_asset.model';
 
 @Table({
   tableName: 'BundleManifest',
@@ -39,4 +45,15 @@ export class BundleManifest extends BaseManifestModel<BundleManifest> {
   })
   @Column(JSON_STRING(DataType.TEXT))
   moduleFederationConfig: ModuleFederationConfig;
+
+  @BelongsToMany(() => BundleAsset, () => BundleManifest_Asset)
+  assets?: BundleAsset[];
+
+  @HasMany(() => BundleManifest_Asset)
+  bundleManifest_asset: BundleManifest_Asset[];
+
+  declare static associations: {
+    assets: BelongsToManyAssociation<BundleManifest, BundleAsset>;
+    bundleManifest_asset: HasManyAssociation<BundleManifest, BundleManifest_Asset>;
+  };
 }
