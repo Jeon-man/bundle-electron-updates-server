c7d861722b887f8f74156acf0f2e88610fb4a4cd
Jeon-man <jhmjhm0106@naver.com>
2023-12-21 14:55:38 +0900

    REFACTOR: Use `DynamicModule`

--- src/modules/multer/multer.module.ts
+++ src/modules/multer/multer.module.ts
@@ -1,17 +1,10 @@
-import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MulterModule } from '@nestjs/platform-express';
 import { MulterConfigService } from './multer.config.service';
 
-@Module({
-  imports: [
-    MulterModule.registerAsync({
-      imports: [ConfigModule],
-      useExisting: MulterConfigService,
-    }),
-  ],
-  exports: [MulterModule],
-})
-export class MulterDynamicModule {}
+export const MulterDynamicModule = MulterModule.registerAsync({
+  imports: [ConfigModule],
+  useClass: MulterConfigService,
+});
 
 export default MulterDynamicModule;
