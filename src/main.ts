cd9c41b737e092bd63be1820b07251435212b0e0
Jeon-man <jhmjhm0106@naver.com>
2023-12-28 15:03:31 +0900

    CHORE: Update cors options

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,8 @@ import fs from 'fs';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: false,
+      origin: '*',
+      credentials: true,
     },
   });
 
